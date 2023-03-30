0481e931119157c553d565f2376b3519e309197f
Jean Cabral <jeancabral@jeans-mbp.lan>
2023-03-29 23:53:32 -0400

    Remove Extra Comments: REFACTOR

--- src/main/java/com/mindex/challenge/data/ReportingStructure.java
+++ src/main/java/com/mindex/challenge/data/ReportingStructure.java
@@ -1,7 +1,5 @@
 package com.mindex.challenge.data;
 
-import java.util.List;
-
 public class ReportingStructure {
 
     private Employee employee;
@@ -16,11 +14,6 @@ public class ReportingStructure {
 
     public void setNumberOfReports(int numberOfReports){
         this.numberOfReports = numberOfReports;
-//        if(employee.getDirectReports() == null){
-//            this.numberOfReports = 0;
-//        } else{
-//           this.numberOfReports = calculateNumberOfReports(employee);
-//        }
     }
 
     public int getNumberOfReports() {
@@ -30,17 +23,5 @@ public class ReportingStructure {
     public Employee getEmployee() {
         return employee;
     }
-
-//    public int calculateNumberOfReports(Employee employee) {
-//        int numberOfReports = 0;
-//        List<Employee> directReports = employee.getDirectReports();
-//        if (directReports != null) {
-//            numberOfReports += directReports.size();
-//            for (Employee directReport : directReports) {
-//                numberOfReports += calculateNumberOfReports(directReport);
-//            }
-//        }
-//        return numberOfReports;
-//    }
 
 }
